90a4d6bc77db9a0e0cc2c9b573e7e1f7772d9df2
Danila Balabaev <shattenjagger@yandex.ru>
2017-08-03 23:08:32 +0300

    Initial

---
+++ func.py
@@ -0,0 +1,16 @@
+def get_users():
+    return []
+
+
+def send_messages():
+    for u in get_users():
+        send_user_message(u)
+
+
+def send_user_message(user):
+    """
+        Not implemented
+    :param user:
+    :return:
+    """
+    return None
